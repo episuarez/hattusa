217ee80c3eccbca8e0473f7bd3ffeecce5df3db0
Epifanio Suárez Martínez <epifanio.suarez@owaristudios.com>
2021-01-29 16:09:29 +0100

    Dark mode

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import threading
 
 import webview
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, redirect, render_template, url_for, request
 
 from configuration import configuration
 from database import db
@@ -16,7 +16,7 @@ library = Library();
 
 @app.route("/", defaults={"page": 1})
 def index(page):
-    return render_template("index.html", books=library.get_books());
+    return render_template("index.html", books=library.get_books(), dark_mode=request.cookies.get("dark_mode"));
 
 @app.route("/synchronization")
 def synchronization():
@@ -36,7 +36,7 @@ def delete_cache():
 @app.route("/viewer/<int:id_book>", defaults={"page": 1})
 @app.route("/viewer/<int:id_book>/<int:page>")
 def viewer(id_book, page):
-    return render_template("viewer.html", id_book=id_book, title=library.get_book_name(id_book), page=page, content=library.get_page_book(id_book, page));
+    return render_template("viewer.html", id_book=id_book, title=library.get_book_name(id_book), page=page, content=library.get_page_book(id_book, page), dark_mode=request.cookies.get("dark_mode"));
 
 def launch_webserver():
     app.run(debug=configuration["DEBUG"]);
